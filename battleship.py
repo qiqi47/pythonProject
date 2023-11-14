1d9286b536a2dc197e28b2890dc350723d1c2d40
Ryotaro (Rio) Takehara <143859347+suisanki@users.noreply.github.com>
2023-11-14 01:03:27 -0600

    Update battleship.py
    The structure of the application in my brain.

--- battleship.py
+++ battleship.py
@@ -1,5 +1,3 @@
-
-
 # Required class:
 # OwnBoard: Define own board
 # Properties and Methods
@@ -12,6 +10,60 @@
 # 'X' for placing and hit battleship
 # ' ' for empty space
 # '-' for missed shot
+
+class Board:
+    def __init__(self):
+        #This initialize board state and shiplist for each instance.
+        self.state = [['']*10 for x in range(10)] 
+        self.shipList = [[]*5 for x in range(5)] 
+        #Originally there should be 5 ships. Those lists will keep the coordination of each ship and delete coordination 
+        #when the place is bombed. When the list turned empty, the ship is considered to be sunk.
+
+    def add(self,length of the ship, coordination,direction):
+        """This method add ship to the board according to the coordination. Length of the ship will be 4, 3(x2), 2(x2),1.
+        Player can select one coordination (like 1A), and then can select which way to extend the ship. For example, if
+        the player select "right" for a ship of length 3, coordination will be 1A,1B,1C. Should throw an error if it is 
+        out of bound."""
+
+    def showShipLocation(self):
+        """This method get infromation from board, and shows where the ships are located, and it's condition (hit or not). 
+        This requires some work, because we don't want to print the board state in a form of a list. Rather, it should 
+        show in form of grid.
+
+    def showBoardState(self):
+        """This method get infromation from board, and shows which part of the board is bombed, and if it is a hit or not. 
+        In the game, this method will be used by an opponent."""
+
+    def bomb(self, coordination):
+        """This method changes status of the board and shiplist after opponent's bombing"""
+
+#Following is the structure of the game that I imagine.
+
+#Get the name of the player 1 and 2 (Maybe not necessary).
+x = input(Player1_name)
+y = input(Player2_name)
+
+while(player 1 and player 2 haven't set their status):
+    while(player 1 haven't set the ships):
+        #player 1 can add ship, and see own map.
+
+    while(player 2 haven't set the ships):
+        #player 2 can add ship, and see own map.
+
+#Setting part is over, real game starts
+
+while (player 1 and player 2 have ship):
+    while(player 1 haven't finished bombing):
+        #player 1 can see own ship, opponent's hit-map, or bomb certain location
+    while(player 2 haven't finished bombing):
+        #player 2 can see own ship, opponent's hit-map, or bomb certain location
+
+print(player X wins)
+
+
+-------------------------------------------------------------------------------------------------------------------------------
+"""I think we are sharing the same idea, but my idea is to implement the board as a class so it will be more concise. 
+ShipList will be a property of the class""" 
 
 from random import randint
 
